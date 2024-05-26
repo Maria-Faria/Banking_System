a2ea98da02f6f2cc6d763b133450a57f98094738
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-05-26 13:16:14 -0300

    editing saque message

--- banking_system.py
+++ banking_system.py
@@ -65,7 +65,7 @@ while(choice != 4):
                 value = float(input("""\nValor inválido! O limite máximo para saque é de R$500,00 e o valor mínimo é de R$1,00! Tente novamente: R$"""))
 
             if(value > balance):
-                print("\nVocê não possui saldo suficiente para realizar o depósito!")
+                print("\nVocê não possui saldo suficiente para realizar o saque!")
 
             else: 
                 print("\nSaque realizado!")
