3356514feb6359c9d1b1a3d78f38dfa7521dce90
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-06-23 14:38:54 -0300

    função sacar

--- banking_system.py
+++ banking_system.py
@@ -1,6 +1,46 @@
 from datetime import date
 import time
 
+def deposit():
+    return 0, 1
+
+def to_withdraw(*, date_last_withdrawal,balance, value, bank_statement, limit, number_withdraws, limit_withdraws):
+
+    if(date_last_withdrawal != date.today()):
+        number_withdraws = 0
+
+    if(number_withdraws == limit_withdraws and date_last_withdrawal == date.today()):
+        print(f"Você atingiu seu limite de {limit_withdraws} saques diários!")
+        time.sleep(3)
+        
+    else: 
+        date_last_withdrawal = date.today()
+
+        value = float(input("""
+        Opção SAQUE selecionada
+                            
+        Digite aqui o valor a ser sacado: R$"""))
+
+        time.sleep(2)
+
+    while(value > limit or value < 0):
+        value = float(input(f"""\nValor inválido! O limite máximo para saque é de R${limit},00 e o valor mínimo é de R$1,00! Tente novamente: R$"""))
+
+    if(value > balance):
+        print("\nVocê não possui saldo suficiente para realizar o saque!")
+
+    else: 
+        print("\nSaque realizado!")
+
+        number_withdraws += 1
+        bank_statement += f"R${value:.2f}; "
+        balance -= value
+
+    return balance, bank_statement, number_withdraws, date_last_withdrawal
+
+def show_bank_statement():
+    return 0
+
 menu = """
 Bem vindo(a) ao Snake Bank!
 *****************************************
@@ -45,36 +85,14 @@ while(choice != 4):
         time.sleep(2)
 
     elif(choice == 2):
+        result = to_withdraw(date_last_withdrawal=date_last_withdrawal,balance=balance, value=0, bank_statement=withdrawals, limit=500, number_withdraws=qt_withdrawals, limit_withdraws=3)
 
-        if(date_last_withdrawal != date.today()):
-            qt_withdrawals = 0
+        balance = result[0]
+        withdrawals = result[1]
+        qt_withdrawals = result[2]
+        date_last_withdrawal = result[3]
 
-        if(qt_withdrawals == 3 and date_last_withdrawal == date.today()):
-            print("Você atingiu seu limite de 3 saques diários!")
-            time.sleep(3)
-        
-        else: 
-            date_last_withdrawal = date.today()
-
-            value = float(input("""
-            Opção SAQUE selecionada
-                                
-            Digite aqui o valor a ser sacado: R$"""))
-
-            while(value > 500 or value < 0):
-                value = float(input("""\nValor inválido! O limite máximo para saque é de R$500,00 e o valor mínimo é de R$1,00! Tente novamente: R$"""))
-
-            if(value > balance):
-                print("\nVocê não possui saldo suficiente para realizar o saque!")
-
-            else: 
-                print("\nSaque realizado!")
-
-                qt_withdrawals += 1
-                withdrawals += f"R${value:.2f}; "
-                balance -= value
-
-            time.sleep(2)
+        time.sleep(2)
 
     elif (choice == 3):
         print(f"""        MEU EXTRATO 
@@ -86,5 +104,8 @@ while(choice != 4):
     
         time.sleep(4)
 
-    elif(choice != 4):
+    elif (choice == 4):
+        print("Obrigado por utilizar nosso sistema!")
+
+    else:
         print("Opção inválida!")
