186f4ed71ca70f5af35dc4af68e3e2e5cde049fe
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-07-08 18:30:33 -0300

    implementando POO - cadastro de usuário OK

--- banking_system.py
+++ banking_system.py
@@ -1,55 +1,214 @@
 from datetime import date
+from abc import ABC, abstractmethod
 import time
 
+## CLASSE CLIENTE
+class Client:
+    def __init__(self, address, accounts = []):
+        self._address = address
+        self._accounts = accounts
+
+    def carry_out_transaction(self, account, transaction):
+        pass
+
+    def add_account(account):
+        pass
+
+class Physical_Person(Client):
+    def __init__(self, cpf, name, birth_date, address, accounts = []):
+        self._cpf = cpf
+        self._name = name
+        self._birth_date = birth_date
+        super().__init__(address, accounts)
+
+    def show_my_data(self):
+        my_accounts = ""
+
+        return f"""
+            Seus dados:
+
+            CPF: {self._cpf}
+            Nome: {self._name}
+            Data de nascimento: {self._birth_date}
+            Endereço: {self._address}
+
+            >>
+
+            Suas contas:
+
+            {self._accounts}
+        """
+    
+    @property
+    def cpf(self):
+        return self._cpf
+
+## CLASSE TRANSAÇÃO
+class Transaction(ABC):
+    @abstractmethod
+    def register(account):
+        pass
+
+class Deposit(Transaction):
+    def __init__(self, value):
+        self._value = value
+
+    def register(account):
+        pass
+
+class To_Withdraw(Transaction):
+    def __init__(self, value):
+        self._value = value
+    def register(account):
+        pass
+## CLASSE HISTÓRICO
+class Historic:
+    def add_transation(self, transation):
+        pass
+## CLASSE CONTA
+class Account:
+    def __init__(self, client, number, agency = "0001", balance = 0, historic = None):
+        self._balance = balance
+        self._historic = historic
+        self._client = client
+        self._number = number
+        self._agency = agency
+    @property
+    def balance(self):
+        return self._balance
+
+    @classmethod
+    def new_account(cls, client, number):
+        return cls(client, number)
+    
+    def to_withdraw(self, *, value):
+        while(value > 500 or value < 0):
+            value = float(input("""\nValor inválido! O limite máximo para saque é de R$500,00 e o valor mínimo é de R$1,00! Tente novamente: R$"""))
+
+        if(value > self._balance):
+            print("\nVocê não possui saldo suficiente para realizar o saque!")
+            return False
+
+        else: 
+            self._balance -= value
+            print("\nSaque realizado!")
+            return True
+        
+    def deposit(self, value, /):
+        while(value <= 0):
+            value = float(input("\nValor inválido! Tente novamente: R$ "))
+
+        print("\nDepósito realizado!")
+
+        return True
+    
+class Current_Account(Account):
+    def __init__(self, client, number, limit = 500, withdrawal_limit = 3, agency = "0001", balance = 0, historic = None):
+        super().__init__(client, number, agency, balance, historic)
+        self._limit = limit
+        self._withdawal_limit = withdrawal_limit
+
+## Testando...
+
+def check_user_exists(cpf):
+    user_exists = False
+    client = None
+
+    for user in clients:
+        if(user.cpf == cpf):
+            user_exists = True
+            client = user
+
+    return user_exists, client
+
+clients = [Physical_Person("49281656833", "Maria", "15/04/2004", "R. Joaquim Antonio da Rocha, 222 - Tinga - Caraguatatuba/SP")]
+
+## Iniciando o sistema
+initial_menu = int(input("""
+Bem vindo(a) ao Snake Bank!
+*****************************************
+
+Você já possui cadastro em nosso banco?
+
+    [1] Sim
+    [2] Não
+
+    => """))
+
+## Usuário ainda não possui cadastro no banco
+if(initial_menu == 2):
+    user_exists = False
+    print("\nOk, vamos criar seu cadastro!\n")
+
+    name = input("Digite seu nome: ")
+    date_of_birth = input("Digite sua data de nascimento: ")
+    cpf = input("Digite seu CPF: ")
+    
+    #verificar se há somente números no CPF
+    while(cpf.isdigit() == False):
+        cpf = input("\nCPF deve conter apenas números, por favor digite novamente: ")
+
+    street = input("Digite o nome da rua onde você mora: ")
+    number = input("Digite o número da sua casa: ")
+    neighborhood = input("Digite o nome do bairro onde você mora: ")
+    city = input("Digite o nome da cidade onde você mora: ")
+    state = input("Digite a sigla do estado onde você mora: ")
+
+    #Verificando sigla do estado
+    while(len(state)> 2):
+        state = input("\nInforme somente a sigla de seu estado: ")
+
+    #Formatando o endereço
+    address = f"{street}, {number} - {neighborhood} - {city}/{state}"
+
+    #Verificar se o CPF já foi cadastrado anteriormente
+    user_exists = check_user_exists(cpf)[0]
+
+    # Caso o CPF não seja encontrado nos resgistros
+    if(not user_exists):
+        client = Physical_Person(address=address, cpf=cpf, name=name, birth_date=date_of_birth)
+        clients.append(client)
+        print("\nParabéns, seu cadastro foi registrado com sucesso!")
+
+    else:
+        client = check_user_exists(cpf)[1]
+        print("\nUsuário já cadastrado!")
+
+    time.sleep(2)
+
+    # Exibindo dados do cliente
+    print(client.show_my_data())
+    
+else:
+    cpf = input("\nDigite seu CPF: ")
+    
+    user_exists = check_user_exists(cpf)[0]
+
+    if(user_exists):
+        client = check_user_exists(cpf)[1]
+        print(client.show_my_data())
+
+    else:
+        print("\nUsuário não encontrado!")
+
+print("**************************************")
+time.sleep(4)
+
+
 def deposit(deposits, balance, /):
-    value = float(input("""
-        Opção DEPÓSITO selecionada
-                      
-        Digite aqui o valor a ser depositado: R$ """))
-
-    while(value <= 0):
-        value = float(input("\nValor inválido! Tente novamente: R$ "))
-
-    print("\nDepósito realizado!")
         
     deposits += f"R${value:.2f}; "
     balance += value
 
     return deposits, balance
 
-def to_withdraw(*, date_last_withdrawal,balance, value, bank_statement, limit, number_withdraws, limit_withdraws):
-
-    if(date_last_withdrawal != date.today()):
-        number_withdraws = 0
-
-    if(number_withdraws == limit_withdraws and date_last_withdrawal == date.today()):
-        print(f"Você atingiu seu limite de {limit_withdraws} saques diários!")
-        time.sleep(3)
-        
-    else: 
-        date_last_withdrawal = date.today()
-
-        value = float(input("""
-        Opção SAQUE selecionada
-                            
-        Digite aqui o valor a ser sacado: R$"""))
-
-        time.sleep(2)
-
-    while(value > limit or value < 0):
-        value = float(input(f"""\nValor inválido! O limite máximo para saque é de R${limit},00 e o valor mínimo é de R$1,00! Tente novamente: R$"""))
-
-    if(value > balance):
-        print("\nVocê não possui saldo suficiente para realizar o saque!")
-
-    else: 
-        print("\nSaque realizado!")
+"""def to_withdraw(*, date_last_withdrawal,balance, value, bank_statement, limit, number_withdraws, limit_withdraws):
 
         number_withdraws += 1
         bank_statement += f"R${value:.2f}; "
         balance -= value
 
-    return balance, bank_statement, number_withdraws, date_last_withdrawal
+    return balance, bank_statement, number_withdraws, date_last_withdrawal"""
 
 def show_bank_statement(balance, /, *, deposits, withdrawals):
     return (f"""        MEU EXTRATO 
@@ -58,30 +217,6 @@ def show_bank_statement(balance, /, *, deposits, withdrawals):
         Saques realizados: {withdrawals}
 
         Saldo atual: R${balance:.2f}""")
-    
-def create_user(users, name, date_of_birth, cpf, street, number, neighborhood, city, state):
-    while(len(state)> 2):
-        state = input("\nInforme somente a sigla de seu estado: ")
-        
-    #verificar se há somente números no CPF
-    while(cpf.isdigit() == False):
-        cpf = input("\nCPF deve conter apenas números, por favor digite novamente: ")
-
-    #verificar se o CPF já foi cadastrado
-    for user in users:
-        if(not cpf in user):
-            #cadastrar novo usuário
-            new_user = {cpf: {
-                "name": name,
-                "date_of_birth": date_of_birth,
-                "address": f"{street}, {number} - {neighborhood} - {city}/{state}"
-            }}
-            users.append(new_user)
-
-            if(users[0] == {}):
-                users.pop(0)
-
-    return users
 
 def create_account(users, accounts, account_number, cpf_user, agency="0001"):
     for user in users:
@@ -99,80 +234,10 @@ def create_account(users, accounts, account_number, cpf_user, agency="0001"):
 
     return accounts
 
-def show_my_data(users, accounts, cpf):
-    my_accounts = ""
-    user_exists = False
-
-    for user in users:
-        if cpf in user:
-            user_exists = True
-            this_user = user[cpf]
-
-    if(user_exists == False):
-        return "Usuário não encontrado!"
-    
-    else:
-        for account in accounts:
-            if cpf in account:
-                my_accounts += f"{account[cpf]["agency"]}-{account[cpf]["account_number"]}\n"
-
-    return f"""
-        Seus dados:
-
-        CPF: {cpf}
-        Nome: {this_user["name"]}
-        Data de nascimento: {this_user["date_of_birth"]}
-        Endereço: {this_user["address"]}
-
-        >>
-
-        Suas contas:
-
-        {my_accounts}
-
-    """
-
 users = [{"49281656833": {"name": "Maria Eduarda de Faria", "date_of_birth": "15/04/2004", "address": f"R. Joaquim Antonio da Rocha, 222 - Tinga - Caraguatatuba/SP"}}]
 accounts = [{}]
 account_number = 1
 user_data = {}
-
-initial_menu = int(input("""
-Bem vindo(a) ao Snake Bank!
-*****************************************
-
-Você já possui cadastro em nosso banco?
-
-    [1] Sim
-    [2] Não
-
-    => """))
-
-if(initial_menu == 2):
-    print("\nOk, vamos criar seu cadastro!\n")
-
-    name = input("Digite seu nome: ")
-    date_of_birth = input("Digite sua data de nascimento: ")
-    cpf = input("Digite seu CPF: ")
-    street = input("Digite o nome da rua onde você mora: ")
-    number = input("Digite o número da sua casa: ")
-    neighborhood = input("Digite o nome do bairro onde você mora: ")
-    city = input("Digite o nome da cidade onde você mora: ")
-    state = input("Digite a sigla do estado onde você mora: ")
-
-    create_user(users, name, date_of_birth, cpf, street, number, neighborhood, city, state)
-
-    print("\nParabéns, seu cadastro foi registrado com sucesso!")
-
-    time.sleep(2)
-    
-else:
-    cpf = input("\nDigite seu CPF: ")
-
-    print(show_my_data(users, accounts, cpf))
-    print("**************************************")
-
-time.sleep(4)
 
 menu = """
     Selecione uma opção:
@@ -210,6 +275,11 @@ while(choice != 5):
         time.sleep(2)
 
     elif(choice == 1):
+        value = float(input("""
+            Opção DEPÓSITO selecionada
+                      
+            Digite aqui o valor a ser depositado: R$ """))
+        
         result = deposit(deposits, balance)
     
         deposits = result[0]
@@ -218,6 +288,24 @@ while(choice != 5):
         time.sleep(2)
 
     elif(choice == 2):
+        if(date_last_withdrawal != date.today()):
+            number_withdraws = 0
+
+        if(number_withdraws == 3 and date_last_withdrawal == date.today()):
+            print("Você atingiu seu limite de 3 aques diários!")
+            time.sleep(3)
+        
+        else: 
+            date_last_withdrawal = date.today()
+
+            value = float(input("""
+            Opção SAQUE selecionada
+                            
+            Digite aqui o valor a ser sacado: R$"""))
+
+            time.sleep(2)
+
+        ## CHAMAR A FUNÇÃO DE SAQUE
         result = to_withdraw(date_last_withdrawal=date_last_withdrawal,balance=balance, value=0, bank_statement=withdrawals, limit=500, number_withdraws=qt_withdrawals, limit_withdraws=3)
 
         balance = result[0]
